ca3385c1587be4fb608844ca087d48f72eeb7199
Cristian Camilo Suarez Camtor <csuare64950@universidadean.edu.co>
2022-03-20 23:59:53 -0500

    Create 1.py

---
+++ taller-nombre/1.py
@@ -0,0 +1,98 @@
+from turtle import*
+title ( 'CAMILO' )
+bgcolor ( 'blue' )
+
+#diseño
+speed ( 3 )
+pencolor ( 'red' )
+pensize ( 5 )
+
+#ubicacion
+penup ()
+goto ( - 400 , - 100 )
+pendown ()
+
+#Letra C
+
+right(160)
+circle(100, 180)
+left(180)
+penup () #no dibuja
+right (190)
+forward ( 20 )
+pendown ()
+
+#letra A
+left ( 80 )
+forward ( 200 )
+right ( 140 )
+forward ( 200 )
+backward ( 100 )
+left ( 140 )
+right ( 70 )
+left ( 180 )
+forward ( 65 )
+penup ()
+backward ( 70 )
+left ( 110 )
+forward ( 100 )
+right ( 270 )
+forward ( 20 )
+pendown ()
+
+
+#letraM
+left (90 )
+forward ( 200 )
+right ( 150 )
+forward ( 90 )
+left ( 120 )
+forward ( 90 )
+left ( 210 )
+forward ( 202 )
+penup () #no dibuja
+right ( 270 )
+forward ( 20 )
+pendown ()
+
+
+#letra I
+right ( 60 )
+forward ( 80 )
+backward ( 40 )
+left ( 90 )
+forward ( 200 )
+right ( 90 )
+forward ( 40 )
+backward ( 40 )
+right ( 180 )
+forward ( 40 )
+penup ()
+backward ( 40 )
+right ( 270 )
+forward ( 200 )
+right ( 270 )
+forward ( 60 )
+pendown ()
+
+#letra l
+right ( 00 )
+forward ( 80 )
+backward ( 40 )
+left ( 90 )
+forward ( 200 )
+left ( 90 )
+forward ( 40 )
+backward ( 40 )
+right ( 270 )
+forward ( 200 )
+right ( 270 )
+forward ( 40 )
+penup ()
+right(0)
+forward(100)
+pendown()
+
+#Letra O
+circle(100)
+
